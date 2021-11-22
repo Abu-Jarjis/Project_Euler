1a038f7fc79a362a724a8a1b799c0d0bb5bc1801
Abu Jarjis <80599115+Abu-Jarjis@users.noreply.github.com>
2021-11-22 14:15:54 +0600

    Update and rename Problems to Question_2.cpp

---
+++ Question_2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int sum_2_least(int numbers[5]);
+int len(int numbers[5]);
+int prime_factor_max(long long int main);
+
+int main()
+{
+    long long int main = 20;
+    cout << prime_factor_max(main);
+    
+}
+
+
+int prime_factor_max(long long int main){
+    for(long long i=2; i<main; i++)
+    {
+        cout<<i << "-------" <<main << "\n";
+        while(main % i==0)
+        {//for yes
+            main /= i;
+
+        }   
+
+    }
