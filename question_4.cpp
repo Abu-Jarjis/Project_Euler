782f5f21e47316c509cac6b31298428d37024a41
Abu Jarjis <80599115+Abu-Jarjis@users.noreply.github.com>
2021-11-23 00:33:31 -0800

    Add files via upload
    Solution to question 4

---
+++ question_4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int is_palindrome(int num);
+int plaindrome_gen(int num);
+
+int main(){
+    int max = 0;
+    for ( int i = 999; i >= 100; i--){
+        for ( int j = 999; j >= 100; j--){
+            int temp = i*j;
+            if (max < temp && is_palindrome(temp)){
+                max = temp;
+            }
+        }
+        
+    }
+    cout << max;
+}
+
+int is_palindrome(int num){
+    int temp = 0;
+    int k = 100;
+    for(int i = num; i > 0; i /= 10){
+        temp = temp*10 + i%10;
+    }
+    return temp == num;
+    
+}
+
